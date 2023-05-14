a9d4e426326ef958d6348bb4dcb4be04d4a3370a
SerStars <sdfs9912@gmail.com>
2023-05-14 23:42:36 +0200

    updated everything

--- StealEmoji/rollup.config.ts
+++ StealEmoji/rollup.config.ts
@@ -2,15 +2,15 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import esbuild from 'rollup-plugin-esbuild';
 import json from '@rollup/plugin-json';
-import copy from 'rollup-plugin-copy';
-import Manifest from './src/manifest.json';
+
+import manifest from './manifest.json';
 import { defineConfig } from 'rollup';
 
 export default defineConfig({
    input: 'src/index.tsx',
    output: [
       {
-         file: `dist/${Manifest.name}.js`,
+         file: `dist/${manifest.name}.js`,
          format: 'cjs',
          strict: false
       },
@@ -19,7 +19,6 @@ export default defineConfig({
       nodeResolve(),
       commonjs(),
       json(),
-      copy({ targets: [{ src: 'src/manifest.json', dest: 'dist/' }] }),
-      esbuild({ minify: true, target: 'ES2019' }),
+      esbuild({ minify: true, target: 'ES2019' })
    ]
 });
